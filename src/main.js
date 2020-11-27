88d2ff7af79af67ccd1d3ca921b870584c11e5d7
chao <zhangxc1120@qq.com>
2020-11-27 11:19:33 +0800

    项目初始化准备

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+import 'normalize.css'
+import '@/style/font/iconfont.css'
+
+Vue.use(vant)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <van-button type="primary">按钮</van-button>
+    <i class="iconfont">&#xe611;</i>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
