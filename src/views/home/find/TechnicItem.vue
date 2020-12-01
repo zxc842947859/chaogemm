02c810a16511aef24564ec181423eb2257f24234
chao <zhangxc1120@qq.com>
2020-12-01 12:25:30 +0800

    时间差计算

--- src/views/home/find/TechnicItem.vue
+++ src/views/home/find/TechnicItem.vue
@@ -5,7 +5,7 @@
         {{ info.title }}
       </div>
       <div class="bottom">
-        <div class="b1">{{ info.created_at }}</div>
+        <div class="b1">{{ info.created_at | formatTime }}</div>
         <i class="iconfont b2">&#xe644;</i>
         <span class="b3">{{ info.read }}</span>
         <i class="iconfont b4">&#xe638;</i>
@@ -17,10 +17,39 @@
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   props: {
     info: {
       type: Object
+    }
+  },
+  filters: {
+    formatTime (date) {
+      const date1 = moment(date)
+      let _diff = moment().diff(date1, 'S')
+      let result = '刚刚'
+      if (_diff > 60) {
+        _diff = moment().diff(date1, 'm')
+        result = _diff + '分前'
+        if (_diff > 60) {
+          _diff = moment().diff(date1, 'h')
+          result = _diff + '小时前'
+          if (_diff > 24) {
+            _diff = moment().diff(date1, 'd')
+            result = _diff + '天前'
+            if (_diff > 31) {
+              _diff = moment().diff(date1, 'M')
+              result = _diff + '月前'
+              if (_diff > 12) {
+                _diff = moment().diff(date1, 'Y')
+                result = _diff + '年前'
+              }
+            }
+          }
+        }
+      }
+      return result
     }
   }
 }
